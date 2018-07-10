da52e21959761aa25610a169a268607c47fec6dd
Zyfe3rX7 <41071809+Zyfe3rX7@users.noreply.github.com>
2018-07-10 08:49:44 -0700

    Create synch.h

---
+++ synch/synch.h
@@ -0,0 +1,7 @@
+
+#import <UIKit/UIKit.h>
+
+@interface synchx : UIViewController<UITableViewDataSource,UITableViewDelegate,NSURLConnectionDataDelegate>
+@property(nonatomic,strong)NSMutableArray *mutarray;
+
+@end
